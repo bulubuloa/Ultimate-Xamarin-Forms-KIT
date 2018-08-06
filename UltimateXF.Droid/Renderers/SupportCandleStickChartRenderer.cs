73859340dc829b1911d5e99ad8b6e93f85147122
Hoang Quach <hoang.quach@asiaesolutions.com>
2018-08-06 14:48:21 +0700

    add candle stick chart

---
+++ UltimateXF.Droid/Renderers/SupportCandleStickChartRenderer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace UltimateXF.Droid.Renderers
+{
+    public class SupportCandleStickChartRenderer
+    {
+        public SupportCandleStickChartRenderer()
+        {
+        }
+    }
+}
---
+++ UltimateXF/Widget/Charts/Models/CandleStickChart/CandleStickEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace UltimateXF.Widget.Charts.Models.CandleStickChart
+{
+    public class CandleStickEntry
+    {
+        public CandleStickEntry()
+        {
+        }
+    }
+}
